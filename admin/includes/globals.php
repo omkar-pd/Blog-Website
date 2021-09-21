d1a42951368b1e1c1dee7a6513b47e36727ab451
omkar-pd <omkarpd109@gmail.com>
2021-09-21 12:57:13 +0530

    #3; Created a single globals file for global variables

---
+++ admin/includes/globals.php
@@ -0,0 +1,18 @@
+<?php 
+global $total_pages,$page,$sql,$errors,$post_id,$isEditingPost,$img,$published,$title,$body,$featured_image,$admin_id,$email,$isEditingUser,$username,$role;
+$total_pages = "";
+$page = "";
+$sql = "";
+$errors = array();
+$post_id = 0;
+$isEditingPost = false;
+$img;
+$published = 0;
+$title = "";
+$body = "";
+$featured_image = "";
+$admin_id = 0;
+$isEditingUser = false;
+$username = "";
+$role = "";
+?>
---
+++ includes/globals.php
@@ -0,0 +1,9 @@
+<?php 
+global $username,$email,$errors,$total_pages,$page,$id,$s,$v,$username_new;
+$username = "";
+$email = "";
+$errors = array();
+$total_pages = "";
+$page = "";
+$id = ""; 
+?>
