aa68749c11debd8d920c25aeeb7e23936165ddb4
omkar-pd <omkarpd109@gmail.com>
2021-09-27 18:19:49 +0530

    #8:Converted plain CSS to SASS

--- includes/head_section.php
+++ includes/head_section.php
@@ -9,5 +9,5 @@
 	<!-- Bootstrap -->
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
 		<!-- Styling for public area -->
-	<link rel="stylesheet" href="static/css/style.css">
+  <link rel="stylesheet" href="static/css/main.css">
 	<meta charset="UTF-8">
