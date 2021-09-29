e2045e72810d5ef188c77e8eb799b1baa932b98a
omkar-pd <omkarpd109@gmail.com>
2021-09-29 16:57:09 +0530

    #21: used forEach loop

--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,12 @@
 const show_pass = document.querySelectorAll(".show_pass");
 const pass_field = document.querySelectorAll(".pass_field");
 
-for (let i = 0; i < show_pass.length; i++) {
-  show_pass[i].addEventListener("click", function () {
-    if (pass_field[i].type == "password") {
-      pass_field[i].type = "text";
+show_pass.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    if (pass_field[index].type == "password") {
+      pass_field[index].type = "text";
     } else {
-      pass_field[i].type = "password";
+      pass_field[index].type = "password";
     }
   });
-}
+});
